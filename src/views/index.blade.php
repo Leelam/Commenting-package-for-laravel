3edc40aaf5fba3611743d9d36c90b5df691e0259
GKrishna M <gm@leelam.com>
2015-11-09 03:17:06 +0530

    Removed jquery and bootstra JS files

--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -34,7 +34,7 @@
 
 
 			<!-- jQuery first, then Bootstrap JS. -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-	<script src="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/js/bootstrap.js"></script>
+	{{--<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	<script src="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/js/bootstrap.js"></script>--}}
 </body>
 </html>
